c8f21f421dffc3d77f1972c25e5b150071e3397c
AryaGGWP <arya.lampung.2016@gmail.com>
2025-07-29 10:03:21 +0700

    push middleware

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express"; 
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.use((req, res) => {
+  res.status(404).send("404 - Page not found");
+}); 
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
